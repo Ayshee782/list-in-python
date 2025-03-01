dc094ab14dbd4934277b804b6986957bef1d2e5a
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-01 21:52:35 +0600

    Add files via upload

---
+++ list practice3.py
@@ -0,0 +1,9 @@
+#Write a Python program to check if an element exists in a list.
+a=[900,56,1,4,9]
+
+b=list(map(int,input("enter list=").split()))
+
+if(a==b):
+    print("ok")
+else:
+    print("no")
